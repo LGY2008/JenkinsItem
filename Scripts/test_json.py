184a69b994917e1f09efbe43978b1213fef80c4d
LGY2008 <150691555@qq.com>
2018-08-19 14:59:38 +0800

    01

--- Scripts/test_json.py
+++ Scripts/test_json.py
@@ -10,5 +10,5 @@ class Test():
     @allure.step(title="测试test_002方法：")
     def test_002(self):
         allure.attach("test002描述","执行打印输出语句")
-        print("test002执行")
+        print("test002执行 ")
         assert 1
